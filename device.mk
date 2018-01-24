6fd0c56f94cca1d50b0b72f5b75542b89dd81b46
Offain <fabian.leutenegger@bluewin.ch>
2018-01-24 20:42:09 +0100

    s2: Adapt ANT+ for CAF
     * CAF wants it this way
    
    Change-Id: I178d2a9903432234b72d5794cc4ac41ff911e077

--- device.mk
+++ device.mk
@@ -51,7 +51,6 @@ $(call inherit-product, $(LOCAL_PATH)/hidl-hals.mk)
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
@@ -92,7 +91,8 @@ PRODUCT_COPY_FILES += \
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
-    com.dsi.ant.antradio_library
+    antradio_app \
+    libantradio
 
 # Audio
 PRODUCT_PACKAGES += \
