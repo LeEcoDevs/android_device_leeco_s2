5d598887ef0ff0bf63a770f97b32af655681f381
Rk779 <Rahulkrishna585@gmail.com>
2019-01-06 13:53:23 +0000

    s2: Tweak init.qcom.power.rc
    * We don't have UFS storage, remove ufs powersaving features
    * Add Cpusets to more services
    * Remove unused services
    
    Change-Id: Ia2274640d6daa73134e1275bfa7228859bcf73a0

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -48,7 +48,7 @@ LOCAL_MODULE_STEM	:= ueventd.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/ueventd.qcom.rc
-LOCAL_MODULE_PATH	:=  $(TARGET_OUT_VENDOR)
+LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
 
 # Configuration scripts
