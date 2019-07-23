f71c769d06bf23db8e1fcfa97c0012a2ad32293b
Rahul Krishna <Rahulkrishna585@gmail.com>
2019-07-23 15:15:15 +0530

    s2: AOSiP Bring-Up

--- aosip.mk
+++ aosip.mk
@@ -17,12 +17,12 @@
 # Inherit device configuration
 $(call inherit-product, device/leeco/s2/full_s2.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSiP stuff.
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := s2
-PRODUCT_NAME := lineage_s2
+PRODUCT_NAME := aosip_s2
 PRODUCT_BRAND := LeEco
 PRODUCT_MANUFACTURER := LeMobile
 
